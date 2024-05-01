50e113d2ee553bd130dcbcd04786e68d048ed802
RhumeCTW <Oghenerhume.Disi@criticaltechworks.com>
2024-05-01 13:06:49 +0100

    Updated the runapp directory

--- dockerfile-demos/runapp/app/Forever.java
+++ dockerfile-demos/runapp/app/Forever.java
@@ -1,4 +1,11 @@
+import lombok.extern.java.Log;
+
+@Log
 public class Forever {
 
-
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Forevere world application");
+        log.info("Run Forever using lombok");
+        Thread.currentThread().join();
+    }
 }
