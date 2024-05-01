5e525d83c11a19d59a52ee182f1b2faedc80571a
RhumeCTW <Oghenerhume.Disi@criticaltechworks.com>
2024-05-01 14:26:59 +0100

    Startup files

---
+++ layerdemo/src/main/java/com/codehacks/layerdemo/LayerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.codehacks.layerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayerdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LayerdemoApplication.class, args);
+    }
+
+}
