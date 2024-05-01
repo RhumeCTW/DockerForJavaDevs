335e1e1da3f2766bea1f4bd9be70b1f21c8d0774
RhumeCTW <Oghenerhume.Disi@criticaltechworks.com>
2024-05-01 12:23:54 +0100

    Refactored code

--- springboot2-6-3/src/main/java/com/codehacks/ServletInitializer.java
+++ springboot2-6-3/src/main/java/com/codehacks/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Springboot263Application.class);
+		return application.sources(com.codehacks.Springboot263Application.class);
 	}
 
 }
